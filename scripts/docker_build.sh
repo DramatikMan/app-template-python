8e20fe4c562a4800cb4ea38762d0ff9a5a9dbb36
Sergey Pavlov <self@sergeypavlov.dev>
2023-12-03 18:27:23 +0300

    Add default build arg values

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker build \
-    --build-arg DOCKER_REGISTRY='https://docker.io/library' \
-    --build-arg PYTHON_INDEX_URL='https://pypi.org/simple' \
     -t app-template-python \
     ./
